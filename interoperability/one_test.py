34e55ec4104489a1e55513553067a5265da860d9
HuguesBouvier <33438817+huguesBouvier@users.noreply.github.com>
2022-09-08 22:35:05 +0000

    test

---
+++ interoperability/one_test.py
@@ -0,0 +1,261 @@
+"""
+*******************************************************************
+  Copyright (c) 2013, 2018 IBM Corp.
+
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of the Eclipse Public License v1.0
+  and Eclipse Distribution License v1.0 which accompany this distribution.
+
+  The Eclipse Public License is available at
+     http://www.eclipse.org/legal/epl-v10.html
+  and the Eclipse Distribution License is available at
+    http://www.eclipse.org/org/documents/edl-v10.php.
+
+  Contributors:
+     Ian Craggs - initial implementation and/or documentation
+*******************************************************************
+"""
+
+import unittest
+
+import mqtt.clients.V5 as mqtt_client, time, logging, socket, sys, getopt, traceback
+import mqtt.formats.MQTTV5 as MQTTV5
+
+class Callbacks(mqtt_client.Callback):
+
+  def __init__(self):
+    self.messages = []
+    self.messagedicts = []
+    self.publisheds = []
+    self.subscribeds = []
+    self.unsubscribeds = []
+    self.disconnects = []
+
+  def __str__(self):
+     return str(self.messages) + str(self.messagedicts) + str(self.publisheds) + \
+        str(self.subscribeds) + str(self.unsubscribeds) + str(self.disconnects)
+
+  def clear(self):
+    self.__init__()
+
+  def disconnected(self, reasoncode, properties):
+    logging.info("disconnected %s %s", str(reasoncode), str(properties))
+    self.disconnects.append({"reasonCode" : reasoncode, "properties" : properties})
+
+  def connectionLost(self, cause):
+    logging.info("connectionLost %s" % str(cause))
+
+  def publishArrived(self, topicName, payload, qos, retained, msgid, properties=None):
+    logging.info("publishArrived %s %s %d %s %d %s", topicName, payload, qos, retained, msgid, str(properties))
+    self.messages.append((topicName, payload, qos, retained, msgid, properties))
+    self.messagedicts.append({"topicname" : topicName, "payload" : payload,
+        "qos" : qos, "retained" : retained, "msgid" : msgid, "properties" : properties})
+    return True
+
+  def published(self, msgid):
+    logging.info("published %d", msgid)
+    self.publisheds.append(msgid)
+
+  def subscribed(self, msgid, data):
+    logging.info("subscribed %d", msgid)
+    self.subscribeds.append((msgid, data))
+
+  def unsubscribed(self, msgid):
+    logging.info("unsubscribed %d", msgid)
+    self.unsubscribeds.append(msgid)
+
+def cleanRetained():
+  callback = Callbacks()
+  curclient = mqtt_client.Client("clean retained".encode("utf-8"))
+  curclient.registerCallback(callback)
+  curclient.connect(host=host, port=port, cleanstart=True)
+  curclient.subscribe(["#"], [MQTTV5.SubscribeOptions(0)])
+  time.sleep(2) # wait for all retained messages to arrive
+  for message in callback.messages:
+    logging.info("deleting retained message for topic", message[0])
+    curclient.publish(message[0], b"", 0, retained=True)
+  curclient.disconnect()
+  time.sleep(.1)
+
+def cleanup():
+  # clean all client state
+  print("clean up starting")
+  clientids = ("myclientid", "myclientid2")
+
+  for clientid in clientids:
+    curclient = mqtt_client.Client(clientid.encode("utf-8"))
+    curclient.connect(host=host, port=port, cleanstart=True)
+    time.sleep(.1)
+    curclient.disconnect()
+    time.sleep(.1)
+
+  # clean retained messages
+  cleanRetained()
+  print("clean up finished")
+
+def usage():
+  logging.info(
+"""
+ -h: --hostname= hostname or ip address of server to run tests against
+ -p: --port= port number of server to run tests against
+ -z: --zero_length_clientid run zero length clientid test
+ -d: --dollar_topics run $ topics test
+ -s: --subscribe_failure run subscribe failure test
+ -n: --nosubscribe_topic_filter= topic filter name for which subscriptions aren't allowed
+
+""")
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+      setData()
+      global callback, callback2, aclient, bclient
+      cleanup()
+
+      callback = Callbacks()
+      callback2 = Callbacks()
+
+      #aclient = mqtt_client.Client(b"\xEF\xBB\xBF" + "myclientid".encode("utf-8"))
+      aclient = mqtt_client.Client("myclientid".encode("utf-8"))
+      aclient.registerCallback(callback)
+
+      bclient = mqtt_client.Client("myclientid2".encode("utf-8"))
+      bclient.registerCallback(callback2)
+
+    def waitfor(self, queue, depth, limit):
+      total = 0
+      while len(queue) < depth and total < limit:
+        interval = .5
+        total += interval
+        time.sleep(interval)
+
+    def test_flow_control1(self):
+      testcallback = Callbacks()
+      # no callback means no background thread, to control receiving
+      testclient = mqtt_client.Client("myclientid".encode("utf-8"))
+
+      # set receive maximum - the number of concurrent QoS 1 and 2 messages
+      clientReceiveMaximum = 2 # set to low number so we can test
+      connect_properties = MQTTV5.Properties(MQTTV5.PacketTypes.CONNECT)
+      connect_properties.ReceiveMaximum = clientReceiveMaximum
+      connect_properties.SessionExpiryInterval = 0
+      connack = testclient.connect(host=host, port=port, cleanstart=True,
+                   properties=connect_properties)
+
+      serverReceiveMaximum = 2**16-1 # the default
+      if hasattr(connack.properties, "ReceiveMaximum"):
+        serverReceiveMaximum = connack.properties.ReceiveMaximum
+
+      receiver = testclient.getReceiver()
+
+      testclient.subscribe([topics[0]], [MQTTV5.SubscribeOptions(2)])
+      receiver.receive(testcallback)
+      self.waitfor(testcallback.subscribeds, 1, 3)
+
+      pubs = 0
+      for i in range(1, clientReceiveMaximum + 2):
+        testclient.publish(topics[0], "message %d" % i, 1)
+        pubs += 1
+
+      # get two publishes
+      acks = 0
+      while True:
+        response1 = MQTTV5.unpackPacket(MQTTV5.getPacket(testclient.sock))
+        if response1.fh.PacketType == MQTTV5.PacketTypes.PUBLISH:
+          break
+        self.assertEqual(response1.fh.PacketType, MQTTV5.PacketTypes.PUBACK)
+        acks += 1
+        del receiver.outMsgs[response1.packetIdentifier]
+      self.assertEqual(response1.fh.PacketType, MQTTV5.PacketTypes.PUBLISH)
+      self.assertEqual(response1.fh.QoS, 1, response1.fh.QoS)
+
+      while True:
+        response2 = MQTTV5.unpackPacket(MQTTV5.getPacket(testclient.sock))
+        if response2.fh.PacketType == MQTTV5.PacketTypes.PUBLISH:
+          break
+        self.assertEqual(response2.fh.PacketType, MQTTV5.PacketTypes.PUBACK)
+        acks += 1
+        del receiver.outMsgs[response2.packetIdentifier]
+      self.assertEqual(response2.fh.PacketType, MQTTV5.PacketTypes.PUBLISH)
+      self.assertEqual(response2.fh.QoS, 1, response1.fh.QoS)
+
+      while acks < pubs:
+        ack = MQTTV5.unpackPacket(MQTTV5.getPacket(testclient.sock))
+        self.assertEqual(ack.fh.PacketType, MQTTV5.PacketTypes.PUBACK)
+        acks += 1
+        del receiver.outMsgs[ack.packetIdentifier]
+
+      with self.assertRaises(socket.timeout):
+        # this should time out because we haven't acknowledged the first one
+        response3 = MQTTV5.unpackPacket(MQTTV5.getPacket(testclient.sock))
+
+      # ack the first one
+      puback = MQTTV5.Pubacks()
+      puback.packetIdentifier = response1.packetIdentifier
+      testclient.sock.send(puback.pack())
+
+      # now get the next packet
+      response3 = MQTTV5.unpackPacket(MQTTV5.getPacket(testclient.sock))
+      self.assertEqual(response3.fh.PacketType, MQTTV5.PacketTypes.PUBLISH)
+      self.assertEqual(response3.fh.QoS, 1, response1.fh.QoS)
+
+      # ack the second one
+      puback.packetIdentifier = response2.packetIdentifier
+      testclient.sock.send(puback.pack())
+
+      # ack the third one
+      puback.packetIdentifier = response3.packetIdentifier
+      testclient.sock.send(puback.pack())
+
+      testclient.disconnect()
+
+def setData():
+  global topics, wildtopics, nosubscribe_topics, host, port
+  topics =  ("TopicA", "TopicA/B", "Topic/C", "TopicA/C", "/TopicA")
+  wildtopics = ("TopicA/+", "+/C", "#", "/#", "/+", "+/+", "TopicA/#")
+  nosubscribe_topics = ("test/nosubscribe",)
+
+
+if __name__ == "__main__":
+  try:
+    opts, args = getopt.gnu_getopt(sys.argv[1:], "h:p:vzdsn:",
+      ["help", "hostname=", "port=", "iterations="])
+  except getopt.GetoptError as err:
+    logging.info(err) # will print something like "option -a not recognized"
+    usage()
+    sys.exit(2)
+
+  iterations = 1
+
+  global topics, wildtopics, nosubscribe_topics, host, topic_prefix
+  topic_prefix = "client_test5/"
+  topics = [topic_prefix+topic for topic in ["TopicA", "TopicA/B", "Topic/C", "TopicA/C", "/TopicA"]]
+  wildtopics = [topic_prefix+topic for topic in ["TopicA/+", "+/C", "#", "/#", "/+", "+/+", "TopicA/#"]]
+  print(wildtopics)
+  nosubscribe_topics = ("test/nosubscribe",)
+
+  host = "frontend1-0.frontend1.default.svc.cluster.local"
+  port = 1883
+  for o, a in opts:
+    if o in ("--help"):
+      usage()
+      sys.exit()
+    elif o in ("-n", "--nosubscribe_topic_filter"):
+      nosubscribe_topic_filter = a
+    elif o in ("-h", "--hostname"):
+      host = a
+    elif o in ("-p", "--port"):
+      port = int(a)
+      sys.argv.remove("-p") if "-p" in sys.argv else sys.argv.remove("--port")
+      sys.argv.remove(a)
+    elif o in ("--iterations"):
+      iterations = int(a)
+
+  root = logging.getLogger()
+  root.setLevel(logging.ERROR)
+
+  logging.info("hostname %s port %d", host, port)
+  print("argv", sys.argv)
+  for i in range(iterations):
+    unittest.main()
